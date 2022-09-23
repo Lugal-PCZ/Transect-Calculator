1bdcef77ee9b0ba1c22f420bd94e45fedd192faf
Paul C. Zimmerman <paul@lugal.com>
2022-09-23 17:18:45 -0400

    Numerous logic and interface improvements.

--- transect_calculator.py
+++ transect_calculator.py
@@ -1,39 +1,83 @@
-# Sensor Array
-trackwidth = 1
-numsensors = 3
+default_numsensors = 5
+default_trackwidth = 1
+default_gridwidth = 50
+default_startx = 0
 
-# Survey Grid
-gridwidth = 45
-startx = 0
+# Input the physical characteristics of the sensor array.
+numsensors = input(
+    f"\nHow many sensors, left-to-right are in your array? [{default_numsensors}] "
+)
+if numsensors == "":
+    numsensors = default_numsensors
+else:
+    numsensors = int(numsensors)
+
+trackwidth = input(
+    f"What is the overall width of your sensor array, from the first sensor to the last one? [{default_trackwidth}] "
+)
+if trackwidth == "":
+    trackwidth = default_trackwidth
+else:
+    trackwidth = int(trackwidth)
+
+
+# Input the width and starting coordinate of the grid.
+gridwidth = input(f"How wide is your survey grid? [{default_gridwidth}] ")
+if gridwidth == "":
+    gridwidth = default_gridwidth
+else:
+    gridwidth = int(gridwidth)
+
+startx = input(
+    f"What is the starting X value of your leftmost sensor? [{default_startx}] "
+)
+if startx == "":
+    startx = default_startx
+else:
+    startx = int(startx)
 
 
 def showtransects():
-    global startx
-    global gridwidth
-    spacing = trackwidth / (numsensors - 1)
-    centerline = startx + (trackwidth / 2)
-    transect = 1
     directions = ["backward", "forward"]
-    print(f"Transect\t#1\tCL\t#{numsensors}")
-    print(f"--------\t--\t--\t--")
-    while startx + trackwidth < gridwidth:
-        direction = directions[transect % 2]
-        if direction == "forward":
-            print(
-                f"{transect} ({direction}):\t{startx}\t{centerline}\t{startx + trackwidth}"
-            )
-        else:
-            print(
-                f"{transect} ({direction}):\t{startx + trackwidth}\t{centerline}\t{startx}"
-            )
-        transect += 1
-        startx += trackwidth + spacing
-        centerline = startx + spacing
+    transectnumber = 1
+    closestsensor = startx
+    print(f"\n{'-'*40}\nNumber of Sensors: {numsensors}")
+    print(f"Track Width: {trackwidth}")
+    print(f"Grid Width: {gridwidth}")
+    print(f"Starting Position of Sensor #1: {startx}\n{'-'*40}")
+    if numsensors == 1:
+        print(f"Transect\t#1")
+        print(f"--------\t--")
+        while closestsensor < gridwidth:
+            transectdirection = directions[transectnumber % 2]
+            print(f"{transectnumber} ({transectdirection}):\t{closestsensor}")
+            transectnumber += 1
+            closestsensor += trackwidth
+    else:
+        spacing = trackwidth / (numsensors - 1)
+        print(f"Transect\t #1\t CL\t #{numsensors}")
+        print(f"--------\t----\t----\t----")
+        while closestsensor + trackwidth < gridwidth:
+            transectdirection = directions[transectnumber % 2]
+            farthestsensor = closestsensor + trackwidth
+            centerline = (closestsensor + farthestsensor) / 2
+            if transectdirection == directions[0]:
+                print(
+                    f"{transectnumber} ({transectdirection}):\t{farthestsensor:.2f}\t{centerline:.2f}\t{closestsensor:.2f}"
+                )
+            else:
+                print(
+                    f"{transectnumber} ({transectdirection}):\t{closestsensor:.2f}\t{centerline:.2f}\t{farthestsensor:.2f}"
+                )
+            transectnumber += 1
+            closestsensor = farthestsensor + spacing
     if gridwidth % numsensors:
         print(
-            f"\nWARNING:\nThe indicated grid width ({gridwidth}m) is not evenly divisible by the number of sensors ({numsensors})."
+            f"\n>>> WARNING <<<\nThe indicated grid width ({gridwidth}) is not evenly divisible by the number of sensors ({numsensors})."
         )
-        print("If you don’t correct this, your survey will have gaps.")
+        print(
+            "If you don’t modify your grid or your sensor array to correct this, your survey will have gaps.\n"
+        )
 
 
 showtransects()
