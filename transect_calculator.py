46615f2bcb7207ed39d26b0aaae9f316f27c2fce
Paul Zimmerman <paul@lugal.com>
2025-08-03 18:05:05 -0400

    Correct type errors in math flagged by pylance.

--- transect_calculator.py
+++ transect_calculator.py
@@ -1,54 +1,50 @@
-default_numsensors = 5
-default_trackwidth = 1
-default_gridwidth = 50
-default_startx = 0
+numsensors = 5
+trackwidth = 1.0
+gridwidth = 50.0
+startx = 0.0
 
 
 def highlight_cyan(thetext):
     return f"\033[1m\033[36m{thetext}\033[0m"
 
+
 def highlight_red(thetext):
     return f"\033[1m\033[31m{thetext}\033[0m"
 
 
-# Input the physical characteristics of the sensor array.
-numsensors = input(
-    f"\nHow many sensors, left-to-right are in your array? [{default_numsensors}] "
-)
-if numsensors == "":
-    numsensors = default_numsensors
-numsensors = int(numsensors)
+def get_parameters():
+    global numsensors
+    global trackwidth
+    global gridwidth
+    global startx
 
-trackwidth = input(
-    f"What is the overall width of your sensor array, from the first sensor to the last one? [{default_trackwidth}] "
-)
-if trackwidth == "":
-    trackwidth = default_trackwidth
-trackwidth = int(trackwidth)
+    # Input the physical characteristics of the sensor array.
+    _ = input(f"\nHow many sensors, left-to-right are in your array? [{numsensors}] ")
+    if _ != "":
+        numsensors = int(_)
+    _ = input(
+        f"What is the overall width of your sensor array, from the first sensor to the last one? [{trackwidth}] "
+    )
+    if _ != "":
+        trackwidth = float(_)
 
-
-# Input the width and starting coordinate of the grid.
-gridwidth = input(f"How wide is your survey grid? [{default_gridwidth}] ")
-if gridwidth == "":
-    gridwidth = default_gridwidth
-gridwidth = int(gridwidth)
-
-startx = input(
-    f"What is the starting X value of your leftmost sensor? [{default_startx}] "
-)
-if startx == "":
-    startx = default_startx
-startx = int(startx)
+    # Input the width and starting coordinate of the grid.
+    _ = input(f"How wide is your survey grid? [{gridwidth}] ")
+    if _ != "":
+        gridwidth = float(_)
+    _ = input(f"What is the starting X value of your leftmost sensor? [{startx}] ")
+    if _ != "":
+        startx = float(_)
 
 
-def showtransects():
+def show_transects():
     directions = [highlight_red("back"), highlight_cyan("away")]
     transectnumber = 1
-    closestsensor = startx
-    print(f"\n{'-'*37}\nNumber of Sensors: {numsensors}")
+    closestsensor = float(startx)
+    print(f"\n{'-' * 37}\nNumber of Sensors: {numsensors}")
     print(f"Track Width: {trackwidth}")
     print(f"Grid Width: {gridwidth}")
-    print(f"Starting Position of Sensor #1: {startx}\n{'-'*37}")
+    print(f"Starting Position of Sensor #1: {startx}\n{'-' * 37}")
     if numsensors == 1:
         print(f"Transect\t#1")
         print(f"--------\t--")
@@ -84,4 +80,5 @@ def showtransects():
         )
 
 
-showtransects()
+get_parameters()
+show_transects()
